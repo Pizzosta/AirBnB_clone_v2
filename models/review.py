21fd831075f5b8ed22edfdd44de40bbc5632c02f
Pizzosta <pizzostor@gmail.com>
2023-05-05 19:14:25 -0700

    Updated Review

--- models/review.py
+++ models/review.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
-
+from models import storage_type
 
 class Review(BaseModel):
     """ Review class to store review information """
