ae68c1c6007fd1f6c640a68864fbe4a7814fffc5
Pizzosta <pizzostor@gmail.com>
2023-05-05 19:23:42 -0700

    Updated

--- models/state.py
+++ models/state.py
@@ -21,14 +21,14 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities 
+        """returns the list of City instances with state_id
+        equals the current State.id
+        FileStorage relationship between State and City
+        """
+        from models import storage
+        related_cities = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                related_cities.append(city)
+        return related_cities
